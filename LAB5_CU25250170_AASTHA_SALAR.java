98ecfb432f7b99c901011b83b04a69207f4d2bfe
Aasthasalar16 <salaraastha25@gmail.com>
2026-08-31 10:36:13 +0530

    Add files via upload

--- LAB5_CU25250170_AASTHA_SALAR.java
+++ LAB5_CU25250170_AASTHA_SALAR.java
@@ -1,39 +1,110 @@
-import java.util.Stack;
+class Node {
+    int data;
+    Node left, right;
 
-public class PostfixEvaluation {
+    Node(int data) {
+        this.data = data;
+    }
+}
 
-    public static void main(String[] args) {
+public class BST {
+    Node root;
 
-        String expression = "23*54*+";
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < expression.length(); i++) {
-
-            char ch = expression.charAt(i);
-
-            if (Character.isDigit(ch)) {
-                stack.push(ch - '0');
-            } else {
-                int b = stack.pop();
-                int a = stack.pop();
-
-                switch (ch) {
-                    case '+':
-                        stack.push(a + b);
-                        break;
-                    case '-':
-                        stack.push(a - b);
-                        break;
-                    case '*':
-                        stack.push(a * b);
-                        break;
-                    case '/':
-                        stack.push(a / b);
-                        break;
-                }
-            }
+    Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
         }
 
-        System.out.println("Result: " + stack.pop());
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    Node delete(Node root, int data) {
+        if (root == null) {
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = delete(root.left, data);
+        } else if (data > root.data) {
+            root.right = delete(root.right, data);
+        } else {
+            if (root.left == null)
+                return root.right;
+
+            if (root.right == null)
+                return root.left;
+
+            root.data = minValue(root.right);
+            root.right = delete(root.right, root.data);
+        }
+
+        return root;
+    }
+
+    int minValue(Node root) {
+        int min = root.data;
+
+        while (root.left != null) {
+            root = root.left;
+            min = root.data;
+        }
+
+        return min;
+    }
+
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    void preorder(Node root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    void postorder(Node root) {
+        if (root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        BST tree = new BST();
+
+        tree.root = tree.insert(tree.root, 50);
+        tree.insert(tree.root, 30);
+        tree.insert(tree.root, 70);
+        tree.insert(tree.root, 20);
+        tree.insert(tree.root, 40);
+        tree.insert(tree.root, 60);
+        tree.insert(tree.root, 80);
+
+        System.out.print("Inorder: ");
+        tree.inorder(tree.root);
+
+        System.out.print("\nPreorder: ");
+        tree.preorder(tree.root);
+
+        System.out.print("\nPostorder: ");
+        tree.postorder(tree.root);
+
+        tree.root = tree.delete(tree.root, 30);
+
+        System.out.print("\nAfter deletion: ");
+        tree.inorder(tree.root);
     }
 }
